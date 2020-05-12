da8b7f17373f43ebf181b7be966bf1a348e7e710
jay5781368 <57901735+jay5781368@users.noreply.github.com>
2020-05-13 00:47:26 +0800

    Create historgramChart.vue
    更新柱状图组件

---
+++ src/views/dv/histogramChart/historgramChart.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+import OptionBar from '../components/OptionBar.vue'
+
+export default {
+    name: 'histogramChart',
+    data() {
+        return{
+            
+        }
+    }
+};
+components:{
+    OptionBar
+}
+</script>
