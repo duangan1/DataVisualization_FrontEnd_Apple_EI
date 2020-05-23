cf75e05e58cab619f988b1d555ec1bc8c58b65fb
duangan1 <ysyyes@163.com>
2020-05-23 10:57:02 +0800

    Update hotmap code

--- src/views/dv/hotMapChart/list.vue
+++ src/views/dv/hotMapChart/list.vue
@@ -66,309 +66,28 @@ export default {
       }
     },
     formatPlotData() {},
-    // getdata(){
-    //     // var _this=this
-    //     this.$axios({
-    //     baseURL: _baseurl,   //重写baseURL
-    //     url: 'api_visual_heat_map',
-    //     method: 'post',
-    //     data: {
-    //       header_id: '1',
-    //     }
-    //   })
-    //   .then(response =>{
-    //     this.data=response.data
-    //     // post 成功，制response.data 为返回的数百据
-    //     // console.log(response.data)
-    //     // return response.data
-    //   })
-    //   .catch(error => {
-    //     // 请求失度败
-    //     console.log(error)
-    //   })
-    // },
 
     initPlot() {
       let chartDv = this.$refs.chartDv;
       let echart = echarts.init(chartDv);
       
-      // 绘制图表
-      // let data={'columns': [1,
-      //   2,
-      //   3,
-      //   4,
-      //   5,
-      //   6,
-      //   7,
-      //   8,
-      //   9,
-      //   10,
-      //   11,
-      //   12,
-      //   13,
-      //   14,
-      //   15,
-      //   16,
-      //   17,
-      //   18,
-      //   19,
-      //   20,
-      //   21,
-      //   22,
-      //   23,
-      //   24,
-      //   25,
-      //   26,
-      //   27,
-      //   28,
-      //   29,
-      //   30,
-      //   31,
-      //   32,
-      //   33,
-      //   34,
-      //   35,
-      //   36,
-      //   37,
-      //   38,
-      //   39,
-      //   40,
-      //   41,
-      //   42,
-      //   43,
-      //   44,
-      //   45,
-      //   46,
-      //   47,
-      //   48,
-      //   49,
-      //   50,
-      //   51,
-      //   52,
-      //   53,
-      //   54,
-      //   55,
-      //   56,
-      //   57,
-      //   58,
-      //   59],
-      //   'index': ['301', '302', '303'],
-      //   'data': [[-4.0,
-      //   -4.0,
-      //   0.0,
-      //   -4.0,
-      //   -4.0,
-      //   -2.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   -4.0,
-      //   -4.0,
-      //   -4.0,
-      //   -2.0,
-      //   2.0,
-      //   4.0,
-      //   0.0,
-      //   4.0,
-      //   -4.0,
-      //   0.0,
-      //   -4.0,
-      //   0.0,
-      //   -2.0,
-      //   -2.0,
-      //   0.0,
-      //   -4.0,
-      //   0.0,
-      //   0.0,
-      //   2.0,
-      //   4.0,
-      //   4.0,
-      //   -4.0,
-      //   -4.0,
-      //   4.0,
-      //   4.0,
-      //   -4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   0.0,
-      //   4.0,
-      //   0.0,
-      //   -2.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   1.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   2.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   -2.0,
-      //   -2.0,
-      //   0.0],
-      //   [4.0,
-      //   4.0,
-      //   0.0,
-      //   -4.0,
-      //   -4.0,
-      //   -2.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   -4.0,
-      //   -4.0,
-      //   -4.0,
-      //   -2.0,
-      //   2.0,
-      //   4.0,
-      //   0.0,
-      //   4.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   -4.0,
-      //   -2.0,
-      //   0.0,
-      //   -4.0,
-      //   0.0,
-      //   0.0,
-      //   2.0,
-      //   4.0,
-      //   4.0,
-      //   -4.0,
-      //   -4.0,
-      //   4.0,
-      //   4.0,
-      //   -4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   0.0,
-      //   4.0,
-      //   0.0,
-      //   -2.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   2.0,
-      //   2.0,
-      //   1.0,
-      //   0.0,
-      //   1.0,
-      //   -2.0,
-      //   1.0,
-      //   0.0,
-      //   -2.0,
-      //   -2.0,
-      //   0.0],
-      //   [-4.0,
-      //   -4.0,
-      //   0.0,
-      //   -4.0,
-      //   -4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   0.0,
-      //   -4.0,
-      //   -4.0,
-      //   -4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   0.0,
-      //   4.0,
-      //   0.0,
-      //   0.0,
-      //   0.0,
-      //   4.0,
-      //   -4.0,
-      //   4.0,
-      //   2.0,
-      //   -4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   -4.0,
-      //   -4.0,
-      //   4.0,
-      //   4.0,
-      //   -4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   0.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   0.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   2.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0,
-      //   4.0]]
-      // }  
-
       let mes = window.location;
       let _baseurl = `//${mes.hostname}:${mes.port}`;
       
-      // axios({
-      //   baseURL: _baseurl,   //重写baseURL
-      //   url: 'api_visual_heat_map/',
-      //   method: 'post',
-      //   data: {
-      //     header_id: '1',
-      //   }
-      // })
-      // .then(response =>{
-      //   this.plotDataAll=response.data
-      //   // post 成功，制response.data 为返回的数百据
-      //   // console.log(response.data)
-      // })
-      // .catch(error => {
-      //   // 请求失度败
-      //   console.log(error)
-      // })
-      
       let data=this.plotDataAll
+
       // console.log(data)
-      let xlabel = data.index
-      let ylabel = data.columns
+      let xlabel = data.machine_no_list
+      let ylabel = data.fai_no_list
       let arr=[]
-      for (let x in xlabel){
-        for (let y in ylabel){
-            arr.push(
-                [xlabel[x],ylabel[y].toString(),data.data[x][y]]
-                );
-        } 
-      }
-      
-      // data.data.map(function (item){
-          // console.log(item[4])
-      // })
+      for (var item of data.data){
+        // console.log(item)
+        arr.push(
+            [item.machine_no, item.fai_no.toString(), item.projected_yields, item.risk2num]
+            );
+      } 
+      // console.log(xlabel)
+      // console.log(ylabel)
 
       let option = {
         tooltip: {
@@ -376,7 +95,7 @@ export default {
             position: 'top',
             formatter:function(params){
               let text;
-              switch (params.data[2]){
+              switch (params.data[3]){
                   case 0:
                       text = 'ok';
                       break;
@@ -408,7 +127,11 @@ export default {
                       text = 'Drawing Reject -';
                       break;
               } 
-              return 'Risk Level: ' + text + '<br/>CNC: ' + params.name + '&nbsp&nbsp&nbsp&nbspFAI NO: ' + params.data[1];
+              return [ 
+                'CNC: ' + params.name + '&nbsp&nbsp&nbsp&nbspFAI NO: ' + params.data[1],
+                'Risk Level :' + text,
+                'Projected Yields: ' + params.data[2]
+              ].join("<br/>");
             },
         },
         animation: false,
